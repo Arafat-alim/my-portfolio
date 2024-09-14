6bb81ddded2d277efa5cf8b98649fc03b39c8083
arafat-alim <arafat97alim@gmail.com>
2024-09-15 01:33:44 +0530

    ✨ Implemented the react helmet.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
 import { NavBar } from "./components";
 
@@ -7,6 +8,32 @@ import "./App.scss";
 const App = () => {
   return (
     <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Dev. Arafat Alim | Full-Stack Developer</title>
+        <meta
+          name="description"
+          content="Arafat Alim is a full-stack web developer specializing in web, software, and analytics solutions. Explore his portfolio and projects."
+        />
+        <meta
+          name="keywords"
+          content="Arafat Alim, full-stack developer, MERN stack, web development, backend, frontend, DevOps, software developer"
+        />
+        <meta
+          property="og:title"
+          content="Dev. Arafat Alim - Full Stack Developer"
+        />
+        <meta
+          property="og:description"
+          content="Arafat Alim, a proficient full-stack developer, specializes in web, software, and analytics solutions. Discover his projects and skills."
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/cocoder/image/upload/v1658092214/Personal/Personal/Github_Profile_fori5o.png"
+        />{" "}
+        {/* Update the image URL */}
+        <meta property="og:url" content="https://dev-arafat.netlify.app/" />
+      </Helmet>
       <NavBar />
       <Header />
       <About />
